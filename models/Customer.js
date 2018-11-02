f987df873156b7aa3b1b05aa2a57c6f768a8941f
phubisz <hubisz@gmail.com>
2018-11-02 15:55:49 +0100

    CRUD app Traversy Media video one

---
+++ models/Customer.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const timestamp = require('mongoose-timestamp');
+
+const CustomerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    balance: {
+        type: Number,
+        default: 0,
+    }
+});
+
+CustomerSchema.plugin(timestamp);
+
+const Customer = mongoose.model('Customer', CustomerSchema);
+
+module.exports = Customer;
---
+++ routes/customers.js
@@ -0,0 +1,91 @@
+const errors = require('restify-errors');
+const Customer = require('../models/Customer');
+
+module.exports = server => {
+// Get all customers
+server.get('/customers', async (req, res, next) => {
+    try {
+    const customers = await Customer.find({});
+    res.send(customers);
+    next();
+    } catch(err) {
+return next(new errors.InvalidArgumentError(err));
+    }
+        });
+
+//Get only one customer by ID
+
+server.get('/customers/:id', async (req, res, next) => {
+try {
+const customer = await Customer.findById(req.params.id)
+res.send(customer);
+next();
+} catch (err) {
+  return next(new errors.ResourceNotFoundError(`There is no customer with id of ${req.params.id}`))
+}
+});
+
+
+
+//Create a customer
+
+server.post('/customers', async (req,res,next) => {
+    //Check for JSON
+    if(!req.is('application/json')) {
+        return next(new errors.InvalidContentError("Expects 'application/json"));
+    }
+
+    const {name, email, balance} = req.body;
+
+    const customer = await new Customer({
+        name,
+        email,
+        balance
+    });
+try {
+    const newCustomer = await customer.save();
+    res.send(201);
+    next();
+} catch(err) {
+ return next(new errors.InternalError(err.message));
+}
+ 
+});
+
+server.put('/customers/:id', async (req, res, next) => {
+
+    if(!req.is('application/json')) {
+        return next(new errors.InvalidContentError("Expects 'application/json"));
+    }
+
+    try {
+
+        const customer = await Customer.findOneAndUpdate({_id: req.params.id}, req.body)
+        res.send(200);
+        next();
+    } catch(err) {
+        return next(new errors.ResourceNotFoundError(`There is no customer with id of ${req.params.id}`))
+    }
+
+});
+
+//Delete customer
+
+server.del('/customers/:id' , async (req, res, next)=> {
+
+    try {
+        const customer = await Customer.findOneAndRemove({_id: req.params.id});
+        res.send(204);
+        next();
+
+    } catch (err) {
+        return next( new errors.ResourceNotFoundError(`There is no customer with id of ${req.params.id}`));
+    }
+
+
+
+});
+
+
+
+}
